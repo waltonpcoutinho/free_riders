b8122c539d27ba5765ed94e6996de79d034d2b49
fliegej <J.Fliege@soton.ac.uk>
2024-11-01 21:02:50 +0000

    Add files via upload

--- checkdom_T1.m
+++ checkdom_T1.m
@@ -1,4 +1,4 @@
-function [bool, Q, valQ, y] = checkdom_T1(P, x, partitions, payoffs, zmax)
+function [bool, Q, valQ, y] = checkdom_T1(P, x, partitions, payoffs, vi_max, zmax)
 %checkdom_T1 Checks if (P, x) can be dominated or not by using the
 %criterion from Theorem 1 (p. 9).
 %
@@ -25,19 +25,20 @@ function [bool, Q, valQ, y] = checkdom_T1(P, x, partitions, payoffs, zmax)
 
 bool=0;
 y=0;
-[n m] = size(P);
+[n, m] = size(P);
 c = zeros(n+1,1);
 c(n+1) = -1;
 
 for i = 1:length(partitions)
     Q = partitions{i};
-    [n m] = size(Q);
+    [n, m] = size(Q);
     valQ = payoffs{i};
+    vi_max_Q = vi_max{i};
     for j = 1:m % sets
         T = Q(:,j);
-        [c, A1, A2, b1, b2] = constructLP_T1(P, x, zmax, Q, T, valQ);
+        [c, A1, A2, b1, b2] = constructLP_T1(P, x, zmax, Q, T, valQ, vi_max_Q);
         [yw, fval, exitflag, output] = linprog(c, A2, b2, A1, b1);
-        fval=-fval;
+        fval=-fval;  
         if ((exitflag>=1) & (fval>0)) 
             % Problem from Th.1 feasible and optimal obj fun value > 0
             bool = 1; % Certificate found that (P, x) can be dominated.
--- construct_imputations.m
+++ construct_imputations.m
@@ -1,29 +1,36 @@
-function [A1 A2 b1 b2] = construct_imputations(P, valP)
-%construct_imputations: takes a permutation P and the values v(S, P) stored
-% in the vector valP and constructs matrices A1, A2, and vectors b1, b2
-% such that x is an imputation if and only if A1*x=b1 and A2*x <= b2.
+function [Aeq, A2, beq, b2] = construct_imputations(P, valP, vi_max_P)
+%construct_imputations: takes a partition P and the values v(S, P) stored
+% in the vector valP and constructs matrices Aeq, A2, and vectors beq, b2
+% such that x is an imputation if and only if Aeq*x=beq and A2*x <= b2.
 
-% Known bugs: lower bounds on x_i are all set to x_i \geq 0.
+% Inputs:
+% P: partition
+% valP: payoff values for all coalitions in P
+% vi_max_P: vector of length n, with vi_max(i) the largest right-hand side
+% showing up in the imputation constraints x_i \geq v( \{ i \} ; .... )
 
 % Author: Joerg Fliege
-% Date: 29/10/2024
+% Date: 01/11/2024
 % Version: 1.0
 % (c) Nucleolus Software Ltd
 
-A1 = P';
-b1 = valP';
+Aeq = P';
+beq = valP';
 % hahahahahahahahaaaaaaaaaaa, genius move, right?
 
+[n m] = size(P);
+A2 = -eye(n);
+b2 = -vi_max_P;
+
 % We are going to build up A2, b2 iteratively:
-
-A2 = [];
-b2 = [];
-
-[n m] = size(P); % m = number of subsets in P
-for s=1:m 
-    subset = P(:,s);
-    A2 = [A2; -diag(subset) ];
-    b2 = [b2; zeros(n,1) ]; % HARDCODED, KNOWN BUG: this means v({i}; ...) = 0.
-end
+%A2 = [];
+%b2 = [];
+%
+%[n m] = size(P); % m = number of subsets in P
+%for s=1:m 
+%    subset = P(:,s);
+%    A2 = [A2; -diag(subset) ];
+%    b2 = [b2; -vi_max_P ]; 
+%end
 
 end
